45c931c25a32571cd635d547bebb1b0aa3b1776b
johnjohnn <conde2010@aol.com>
2021-11-16 21:40:16 -0500

    adding some javascript

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const countdownForm = document.getElementById("countdownForm");
 const dateEl = document.getElementById("date-picker");
 
 
+let countdownTitle = "";
+let countdownDate = "";
+let countdownValue = Date;
 
 // Set Date Input Min with Todays's Date 
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+// Take Valudes from Input 
+function updateCountdown (e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElemtn[1].value;
+    console.log(countdownTitle, countdownDate);
+
+    // Get number Version of current Date ,  update DOM 
+    countdownValue = new Date(countdownDate).getTime();
+    console.log("countdown value:", countdownValue);
+
+
+}
+
+
+// Event Listener 
+countdownForm.addEventListener("submit", updateCountdown);
